cde0d38f88b2f12aecd64207183aae8873e5ffbb
Michael Silvanovich <silvanovich.michael@gmail.com>
2022-06-19 21:59:42 +0300

    Fixed bot reference propagation

--- src/main/kotlin/ru/silvmike/bot/dispatcher/SimpleBotConfigurer.kt
+++ src/main/kotlin/ru/silvmike/bot/dispatcher/SimpleBotConfigurer.kt
@@ -2,11 +2,12 @@ package ru.silvmike.bot.dispatcher
 
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.dispatch
+import ru.silvmike.bot.config.postprocessing.BotAware
 
 class SimpleBotConfigurer(
     private val botToken: String,
     private val dispatcherConfigurers: List<DispatcherConfigurer>
-) : BotConfigurer {
+) : BotConfigurer, BotAware {
 
     override fun configure(builder: Bot.Builder) {
 
@@ -18,5 +19,10 @@ class SimpleBotConfigurer(
         }
     }
 
+    override fun setBot(bot: Bot) {
+        dispatcherConfigurers.forEach {
+            if (it is BotAware) it.setBot(bot)
+        }
+    }
 
 }
